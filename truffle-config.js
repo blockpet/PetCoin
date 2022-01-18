9c2836bb95016659a6e866aa2ddd19ab97d14bd2
Sangwon Lee <emicree@gmail.com>
2022-01-18 12:57:25 +0900

    truffle을 이용한 compile, deploy 기능 개선

--- truffle-config.js
+++ truffle-config.js
@@ -1,19 +1,17 @@
 const config = require("config");
-const HDWalletProvider = require("@truffle/hdwallet-provider");
+const HDWalletProvider = require("truffle-hdwallet-provider-klaytn");
+const Caver = require("caver-js");
 
 const GAS_LIMIT = 8500000;
 const GAS_PRICE = null;
 
-const { owner, testnet, mainnet } = config || {};
+const { owner, devnet, testnet, mainnet, kas } = config || {};
 
 module.exports = {
   networks: {
-    development: {
-      // host: "localhost",
-      // port: 8545,
-      // network_id: "*",
-      provider: () => new HDWalletProvider(owner.privateKey, testnet.url),
-      network_id: testnet.networkId,
+    devnet: {
+      provider: () => new HDWalletProvider(owner.privateKey, devnet.url),
+      network_id: devnet.networkId,
       gas: GAS_LIMIT,
       gasPrice: GAS_PRICE,
     },
@@ -26,6 +24,58 @@ module.exports = {
     mainnet: {
       provider: () => new HDWalletProvider(owner.privateKey, mainnet.url),
       network_id: mainnet.network_id,
+      gas: GAS_LIMIT,
+      gasPrice: GAS_PRICE,
+    },
+    kasBaobab: {
+      provider: () =>
+        new HDWalletProvider(
+          owner.privateKey,
+          new Caver.providers.HttpProvider(
+            "https://node-api.klaytnapi.com/v1/klaytn",
+            {
+              headers: [
+                {
+                  name: "Authorization",
+                  value:
+                    "Basic " +
+                    Buffer.from(
+                      kas.accessKeyId + ":" + kas.secretAccessKey
+                    ).toString("base64"),
+                },
+                { name: "x-chain-id", value: testnet.networkId },
+              ],
+              keepAlive: false,
+            }
+          )
+        ),
+      network_id: testnet.networkId,
+      gas: GAS_LIMIT,
+      gasPrice: GAS_PRICE,
+    },
+    kasCypress: {
+      provider: () =>
+        new HDWalletProvider(
+          owner.privateKey,
+          new Caver.providers.HttpProvider(
+            "https://node-api.klaytnapi.com/v1/klaytn",
+            {
+              headers: [
+                {
+                  name: "Authorization",
+                  value:
+                    "Basic " +
+                    Buffer.from(
+                      kas.accessKeyId + ":" + kas.secretAccessKey
+                    ).toString("base64"),
+                },
+                { name: "x-chain-id", value: mainnet.networkId },
+              ],
+              keepAlive: false,
+            }
+          )
+        ),
+      network_id: mainnet.networkId,
       gas: GAS_LIMIT,
       gasPrice: GAS_PRICE,
     },
